765a5b35510c97b6590f22578526fafe01c59aea
Тимур Сидорюк <skysidtim@gmail.com>
2021-08-31 17:08:59 +0700

    make questions template.
    Write README
    Setup version number
    Fill other metadata

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
-import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, Plugin, PluginSettingTab, Setting, TFile} from 'obsidian';
 
 interface MyPluginSettings {
 	filename: string;
+	questionsTemplate: string;
 }
 const DEFAULT_FILENAME = "RandomStructuralDiaryQuestions";
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	filename: DEFAULT_FILENAME
+	filename: '',
+	questionsTemplate: ''
 }
 
-const NUM_OF_QUESTIONS = 2;
 
 
 
@@ -19,8 +20,6 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		this.addStatusBarItem().setText('Status Bar Text');
-
 		this.addCommand({
 			id: 'create-questions-list',
 			name: 'Create questions list',
@@ -29,30 +28,22 @@ export default class MyPlugin extends Plugin {
 				let leaf = this.app.workspace.activeLeaf;
 				if (leaf) {
 					if (!checking) {
-						let file = this.app.vault.getMarkdownFiles().find(x => x.name.contains(this.settings.filename));
+						let file = this.app.vault.getMarkdownFiles().find(x => x.name === `${this.settings.filename}.md`);
 						if(!file){
 							const pathToDefaultFile = this.manifest.dir + `/${DEFAULT_FILENAME}.md`;
 							const adapter = this.app.vault.adapter;
 							adapter.read(pathToDefaultFile).then(result => {
-								console.log(this.settings);
-								this.app.vault.create(`${this.settings.filename}.md`, result)
+								let filename = this.settings.filename == '' ? DEFAULT_FILENAME : this.settings.filename;
+								this.app.vault.create(`${filename}.md`, result)
 									.then(createdFile => {
-										file = createdFile;
+										this.fillFileWithQuestions(createdFile);
 									});
 							});
 						}
-						this.app.vault.read(file).then(result =>
-							{
-								let sections = this.getSections(result);
-								let questions = sections.map(x => this.generateRandomQuestionsFromSection(x, NUM_OF_QUESTIONS), this);
-								let flattenQuestions = questions.reduce((acc, val) => acc.concat(val), []);
+						else {
+							this.fillFileWithQuestions(file);
+						}
 
-								let outputString = flattenQuestions.join("\n\n\n");
-
-								let file = this.app.workspace.getActiveFile();
-								this.app.vault.modify(file, outputString);
-							}
-						);
 					}
 					return true;
 				}
@@ -72,6 +63,28 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+
+	private fillFileWithQuestions(file: TFile): void{
+		this.app.vault.read(file).then(result =>
+			{
+				let sections = this.getSections(result);
+				let questionsSettings = this.parseQuestionSettings();
+				let questions = sections.map(x => {
+					let numOfQuestions = questionsSettings.get(sections.indexOf(x) + 1);
+					if(!numOfQuestions)
+						numOfQuestions = this.getRandomInt(x.length);
+
+					return this.generateRandomQuestionsFromSection(x, numOfQuestions);
+				}, this);
+				let flattenQuestions = questions.reduce((acc, val) => acc.concat(val), []);
+
+				let outputString = flattenQuestions.join("\n\n\n");
+
+				let file = this.app.workspace.getActiveFile();
+				this.app.vault.modify(file, outputString);
+			}
+		);
 	}
 
 	/**
@@ -137,6 +150,28 @@ export default class MyPlugin extends Plugin {
 		let randomNumber = this.getRandomInt(questions.length);
 		return questions[randomNumber];
 	}
+
+	/**
+	 * Prepare settings for using
+	 * @private
+	 */
+	private parseQuestionSettings(): Map<number, number>{
+		let result: Map<number, number> = new Map<number, number>();
+
+		if(!this.settings.questionsTemplate)
+			return result;
+
+		let splitedSettings = this.settings.questionsTemplate.split(';');
+		splitedSettings.map(x => {
+			let splitedValues = x.split('-');
+			let sectionNumber = splitedValues[0];
+			let numberOfQuestions = splitedValues[1];
+
+			result.set(Number(sectionNumber), Number(numberOfQuestions));
+		})
+
+		return result;
+	}
 }
 
 class SampleSettingTab extends PluginSettingTab {
@@ -156,12 +191,22 @@ class SampleSettingTab extends PluginSettingTab {
 
 			new Setting(containerEl)
 				.setName('Path to questions file')
-				.setDesc('Path to questions file')
 				.addText(text => text
-					.setPlaceholder(DEFAULT_FILENAME)
-					.setValue(DEFAULT_FILENAME)
-					.onChange(async (value) => {
-						this.plugin.settings.filename = value;
+					.setPlaceholder('FileName')
+					.setValue(this.plugin.settings.filename)
+					.onChange(async (new_filename) => {
+						this.plugin.settings.filename = new_filename;
+						await this.plugin.saveSettings();
+					}));
+
+			new Setting(containerEl)
+				.setName('Questions Template')
+				.setDesc('Format: section1-numberOfQuestions;section1-numberOfQuestions; 1-3;2-2;...\n If section not specified picks random number of questions')
+				.addText(text => text
+					.setPlaceholder('1-3;2-2;')
+					.setValue(this.plugin.settings.questionsTemplate)
+					.onChange(async (new_template) => {
+						this.plugin.settings.questionsTemplate = new_template;
 						await this.plugin.saveSettings();
 					}));
 		}
