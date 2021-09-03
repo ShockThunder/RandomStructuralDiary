4fc1bafcb477d556ef54a540a1683e6c90eac258
Тимур Сидорюк <skysidtim@gmail.com>
2021-09-03 11:20:06 +0700

    fix file not exists bug

--- main.ts
+++ main.ts
@@ -33,9 +33,7 @@ export default class RandomStructuralDiaryPlugin extends Plugin {
                     await this.fillFileWithQuestions(fileContent);
                 }
                 else {
-                    const pathToDefaultFile = this.manifest.dir + `/${DEFAULT_FILENAME}.${MARKDOWN_EXTENSION}`;
-                    let defaultFile = await this.app.vault.adapter.read(pathToDefaultFile);
-                    await this.fillFileWithQuestions(defaultFile);
+                    await this.fillFileWithQuestions(DEFAULT_QUESTIONS);
                 }
             }
         });
@@ -220,3 +218,164 @@ class SampleSettingTab
                 }));
     }
 }
+
+const DEFAULT_QUESTIONS =
+    "# Remembering important events\n" +
+    "\n" +
+    "What important things happened  today?\n" +
+    "\n" +
+    "What places did I visit?\n" +
+    "\n" +
+    "With whom did I meet/speak?\n" +
+    "\n" +
+    "What was the topic of conversations?\n" +
+    "\n" +
+    "What did I purchase?\n" +
+    "\n" +
+    "What important did I do, start to do, accomplished?\n" +
+    "\n" +
+    "# Introspection (To sum up every project/day/week/month/year)\n" +
+    "\n" +
+    "What was successful or unsuccessful?\n" +
+    "\n" +
+    "Why?\n" +
+    "\n" +
+    "What could be done in more simple, easy and efficient way?\n" +
+    "\n" +
+    "What things weren't accomplished?\n" +
+    "\n" +
+    "What did hinder me?\n" +
+    "\n" +
+    "# Rationality\n" +
+    "\n" +
+    "Did I think thoroughly before speak/act? Did I weight consequences?\n" +
+    "\n" +
+    "What were wrong or irrational in my actions, words, thoughts?\n" +
+    "\n" +
+    "What would be the better way?\n" +
+    "\n" +
+    "What was the reason for my misstep?\n" +
+    "\n" +
+    "What should I do in order to not repeat my mistake?\n" +
+    "\n" +
+    "# Patterns\n" +
+    "\n" +
+    "What are my most common failures?\n" +
+    "\n" +
+    "What virtues were forgotten?\n" +
+    "\n" +
+    "What responsibilities are neglected?\n" +
+    "\n" +
+    "# Time-managment\n" +
+    "\n" +
+    "Did I spend time efficiently?\n" +
+    "\n" +
+    "What was time wasting?\n" +
+    "\n" +
+    "How can I optimise my day?\n" +
+    "\n" +
+    "What resources do I have?\n" +
+    "\n" +
+    "Am I using them effectively?\n" +
+    "\n" +
+    "Did I procrastinate?\n" +
+    "\n" +
+    "# Future\n" +
+    "\n" +
+    "Are there potential areas of crisis in my life?\n" +
+    "\n" +
+    "What am I doing to prepare?\n" +
+    "\n" +
+    "Is there opportunities for future development?\n" +
+    "\n" +
+    "# Emotions\n" +
+    "\n" +
+    "Did I keep peaceful spirit (Inner peace)?\n" +
+    "\n" +
+    "What is the most frequent topic of my thoughts?\n" +
+    "\n" +
+    "What thoughts are troubling me?\n" +
+    "\n" +
+    "What is the source of bad feelings?\n" +
+    "\n" +
+    "Are there any fearful, worrisome, upsetting things?\n" +
+    "\n" +
+    "Do I feel guilty, angry, offended, unsure?\n" +
+    "\n" +
+    "Why do I feel this way?\n" +
+    "\n" +
+    "What was the source of joy and pleasure for me recently?\n" +
+    "\n" +
+    "Why?\n" +
+    "\n" +
+    "# Intuition\n" +
+    "\n" +
+    "Do I have the feeling that something is wrong?\n" +
+    "\n" +
+    "Do I have the feeling that I missed something?\n" +
+    "\n" +
+    "Did I have an impulse to do something?\n" +
+    "\n" +
+    "What are opportunities for me in these circumstances to serve God and neighbors?\n" +
+    "\n" +
+    "Did I saw in a dream something meaningful?\n" +
+    "\n" +
+    "# Self-actualization\n" +
+    "\n" +
+    "What are my priorities for this period of life?\n" +
+    "\n" +
+    "What are my values?\n" +
+    "\n" +
+    "What are my dreams?\n" +
+    "\n" +
+    "What is my weakest point in character, knowledge, skills, habits?\n" +
+    "\n" +
+    "What is my strongest point?\n" +
+    "\n" +
+    "What makes me happy and gratified?\n" +
+    "\n" +
+    "# Intellectual life\n" +
+    "\n" +
+    "All ideas, aha reactions, eurekas, observations go here.\n" +
+    "\n" +
+    "What books/articles have I started to read or have finished to read?\n" +
+    "\n" +
+    "What texts am I working on?\n" +
+    "\n" +
+    "What have l finished?\n" +
+    "\n" +
+    "In what conferences and meetings did I participate?\n" +
+    "\n" +
+    "What movies have I watched?\n" +
+    "\n" +
+    "What new information have I discovered from books or conversations?\n" +
+    "\n" +
+    "What information needs to be checked?\n" +
+    "\n" +
+    "What topics do I need to consider more carefully and in depth?\n" +
+    "\n" +
+    "What facts are against my point of view?\n" +
+    "\n" +
+    "Is there reasonable doubt?\n" +
+    "\n" +
+    "# Social life\n" +
+    "\n" +
+    "Did I practice mindful listening?\n" +
+    "\n" +
+    "What new information have I found out about my acquaintance and friends?\n" +
+    "\n" +
+    "What are their dreams, problems, virtues, prayer needs?\n" +
+    "\n" +
+    "How can I participate in their life?\n" +
+    "\n" +
+    "Was I thankful?\n" +
+    "\n" +
+    "# Prayers\n" +
+    "\n" +
+    "What are my confessions to God?\n" +
+    "\n" +
+    "gratitudes to God?\n" +
+    "\n" +
+    "asking to God?\n" +
+    "\n" +
+    "What answers have I received?"
